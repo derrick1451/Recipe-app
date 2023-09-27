67887d209d9eec4bf3a696c09cdb7faf1d46dfd1
Derrick1451 <haylederrick14@gmail.com>
2023-09-27 12:33:54 +0300

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_134707) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_085811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_134707) do
     t.string "name"
     t.integer "price"
     t.integer "quantity"
-    t.integer "measurement_unit"
+    t.string "measurement_unit"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
