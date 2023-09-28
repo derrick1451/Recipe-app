4015f59054f11687385ad5463f1b09e2eb9a1138
Derrick1451 <haylederrick14@gmail.com>
2023-09-28 11:05:05 +0300

    Update required changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "foods#index"
   resources :foods,only: [:index, :show, :create, :new]
+  resources :recipes, only: [:index,:show, :destroy, :create, :new] do
+    member do
+      patch 'toggle_public'
+    end  
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
 end
