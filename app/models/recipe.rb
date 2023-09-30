b58dc230e41666d005bcaec63f31a1371c0e3d00
Derrick1451 <haylederrick14@gmail.com>
2023-09-30 05:22:51 +0300

    Edit model files

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,10 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
+  validates :name, presence: true
+  validates :preparation_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def total_food_items
     recipe_foods.sum(:quantity)
   end
